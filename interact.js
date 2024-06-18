cf90ef801b301abe8fa8c0b38a4fd74a007a32d2
ChairmanMorais <160012331+ChairmanMorais@users.noreply.github.com>
2024-06-18 17:23:44 +0200

    Add files via upload

---
+++ interact.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Website loaded');
+
+    // Get the button element
+    const humanBt = document.getElementById('humanBt');
+
+    // Add an event listener for the button click
+    myButton.addEventListener('click', () => {
+        alert('Button was clicked!');
+    });
+});
